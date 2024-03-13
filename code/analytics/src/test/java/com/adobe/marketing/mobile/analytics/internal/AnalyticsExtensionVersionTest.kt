79d785b2053b0f74ff40cad0bb6a64ccaa5da51e
Calise Cheung <cacheung@adobe.com>
2024-03-13 16:28:08 -0700

    Update extension_version back to 3.0.0 for beta 1 release (#80)
    * Update extension_version back to 3.0.0
    
    Update extension_version back to 3.0.0
    
    * Ignore the extension version unit test
    
    Ignore the extension version unit test
    
    * remove the version test
    
    remove the version test, it will be done in the github action.
    
    * update format
    
    update format

--- code/analytics/src/test/java/com/adobe/marketing/mobile/analytics/internal/AnalyticsExtensionVersionTest.kt
+++ code/analytics/src/test/java/com/adobe/marketing/mobile/analytics/internal/AnalyticsExtensionVersionTest.kt
@@ -13,47 +13,15 @@ package com.adobe.marketing.mobile.analytics.internal
 
 import com.adobe.marketing.mobile.Analytics
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
-import java.io.FileInputStream
-import java.util.Properties
 
 @RunWith(MockitoJUnitRunner.Silent::class)
 class AnalyticsExtensionVersionTest {
-    private val PROPERTY_MODULE_VERSION = "moduleVersion"
 
     @Test
     fun internalExtensionVersion_publicExtensionVersion_asEqual() {
         assertEquals(AnalyticsConstants.EXTENSION_VERSION, Analytics.extensionVersion())
-    }
-
-    @Test
-    fun extensionVersion_verifyModuleVersionInPropertiesFile_asEqual() {
-        val properties: Properties = loadGradleProperties()
-        assertNotNull(Analytics.extensionVersion())
-        assertFalse(Analytics.extensionVersion().isEmpty())
-        val moduleVersion: String = properties.getProperty(PROPERTY_MODULE_VERSION)
-        assertNotNull(moduleVersion)
-        assertFalse(moduleVersion.isEmpty())
-        assertEquals(
-            java.lang.String.format(
-                "Expected version to match in gradle.properties (%s) and extensionVersion API (%s)",
-                moduleVersion,
-                Analytics.extensionVersion()
-            ),
-            moduleVersion,
-            Analytics.extensionVersion()
-        )
-    }
-
-    private fun loadGradleProperties(): Properties {
-        val properties = Properties()
-
-        val gradlePropertiesPath = "../gradle.properties"
-        FileInputStream(gradlePropertiesPath).use { properties.load(it) }
-        return properties
     }
 }
