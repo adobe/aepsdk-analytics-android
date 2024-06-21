35d8ae009c17d7158a7c83e3060c55edbaf295af
Kevin Lind <lind@adobe.com>
2024-06-21 15:22:18 -0700

    Add functional test using full map of Places shared state to validate type casting/parsing.

--- code/analytics/src/test/java/com/adobe/marketing/mobile/analytics/function/test/AnalyticsTrackPlacesTests.kt
+++ code/analytics/src/test/java/com/adobe/marketing/mobile/analytics/function/test/AnalyticsTrackPlacesTests.kt
@@ -97,7 +97,100 @@ internal class AnalyticsTrackPlacesTests : AnalyticsFunctionalTestBase() {
             "a.loc.poi.id" to "myRegionId",
             "a.loc.poi" to "myRegionName"
         )
-        Assert.assertTrue(expectedContextData == contextDataMap)
+        Assert.assertEquals(expectedContextData, contextDataMap)
+        Assert.assertEquals(expectedVars.size, varMap.size)
+        Assert.assertEquals(expectedVars, varMap)
+    }
+
+    @Test(timeout = 10000)
+    fun `analytics hit contains current poi places data`() {
+        val countDownLatch = CountDownLatch(1)
+        var varMap: Map<String, Any> = emptyMap()
+        var contextDataMap: Map<String, Any> = emptyMap()
+        monitorNetwork { request ->
+            if (request.url.startsWith("https://test.com/b/ss/rsid/0/")) {
+                val body = URLDecoder.decode(String(request.body), "UTF-8")
+                varMap = extractQueryParamsFrom(body)
+                contextDataMap = extractContextDataFrom(body)
+                countDownLatch.countDown()
+            }
+        }
+
+        val analyticsExtension = initializeAnalyticsExtensionWithPreset(
+            mapOf(
+                "analytics.server" to "test.com",
+                "analytics.rsids" to "rsid",
+                "global.privacy" to "optedin",
+                "experienceCloud.org" to "orgid",
+                "analytics.batchLimit" to 0,
+                "analytics.offlineEnabled" to true,
+                "analytics.backdatePreviousSessionInfo" to true,
+                "analytics.launchHitDelay" to 1
+            ),
+            defaultIdentity()
+        )
+
+        // Test with full shared state to validate parsing/casting of types
+        updateMockedSharedState(
+            "com.adobe.module.places",
+            mapOf(
+                "currentpoi" to mapOf(
+                    "regionid" to "99306680-a0e5-49f1-b0eb-c52c6e05ce01",
+                    "useriswithin" to true,
+                    "latitude" to 37.3309257,
+                    "libraryid" to "311cbfb0-ac5e-436a-b22d-4a917426880d",
+                    "regionname" to  "Adobe 100",
+                    "weight" to 1,
+                    "regionmetadata" to mapOf(
+                        "country" to "Adobe 100",
+                        "city" to "Adobe 100",
+                        "street" to "Adobe 100",
+                        "state" to "Adobe 100",
+                        "category" to "Adobe 100"
+                    ),
+                    "radius" to 100,
+                    "longitude" to -121.8939791
+                )
+            )
+        )
+
+        val trackEvent = Event.Builder(
+            "track event",
+            EventType.GENERIC_TRACK,
+            EventSource.REQUEST_CONTENT
+        ).setEventData(
+            mapOf(
+                "action" to "testActionName",
+                "contextdata" to mapOf(
+                    "k1" to "v1",
+                    "k2" to "v2"
+                )
+            )
+        ).build()
+
+        analyticsExtension.handleIncomingEvent(trackEvent)
+
+        countDownLatch.await()
+        val expectedVars: Map<String, String> = mapOf(
+            "ndh" to "1",
+            "ce" to "UTF-8",
+            "cp" to "foreground",
+            "pev2" to "AMACTION:testActionName",
+            "pe" to "lnk_o",
+            "mid" to "mid",
+            "aamb" to "blob",
+            "aamlh" to "lochint",
+            "t" to TimeZoneHelper.TIMESTAMP_TIMEZONE_OFFSET,
+            "ts" to trackEvent.timestampInSeconds.toString()
+        )
+        val expectedContextData: Map<String, String> = mapOf(
+            "k1" to "v1",
+            "k2" to "v2",
+            "a.action" to "testActionName",
+            "a.loc.poi.id" to "99306680-a0e5-49f1-b0eb-c52c6e05ce01",
+            "a.loc.poi" to "Adobe 100"
+        )
+        Assert.assertEquals(expectedContextData, contextDataMap)
         Assert.assertEquals(expectedVars.size, varMap.size)
         Assert.assertEquals(expectedVars, varMap)
     }
@@ -184,7 +277,7 @@ internal class AnalyticsTrackPlacesTests : AnalyticsFunctionalTestBase() {
             "a.loc.poi.id" to "myRegionId2",
             "a.loc.poi" to "myRegionName2"
         )
-        Assert.assertTrue(expectedContextData == contextDataMap)
+        Assert.assertEquals(expectedContextData, contextDataMap)
         Assert.assertEquals(expectedVars.size, varMap.size)
         Assert.assertEquals(expectedVars, varMap)
     }
